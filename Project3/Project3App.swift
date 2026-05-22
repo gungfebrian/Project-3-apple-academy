ee11d77c47d933d46a71663ba3586d1beee3c2b4
Gung <guefef17@gmail.com>
2026-05-22 15:46:23 +0800

    Initial Commit

---
+++ Project3/Project3App.swift
@@ -0,0 +1,17 @@
+//
+//  Project3App.swift
+//  Project3
+//
+//  Created by Gung  on 22/05/26.
+//
+
+import SwiftUI
+
+@main
+struct Project3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
